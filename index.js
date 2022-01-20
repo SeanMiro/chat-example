dfc3321c1734eb22160d230df7900401ad3e0dea
SeanMiro <seanwinters@Seans-MacBook-Pro.local>
2022-01-20 14:20:47 +0100

    cors

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const app = require('express')();
 const http = require('http').Server(app);
-const io = require('socket.io')(http);
+const io = require("socket.io")(http, {
+  cors: {
+    origin: "https://localhost:3000",
+    methods: ["GET", "POST"],
+    allowedHeaders: ["my-custom-header"],
+    credentials: true
+  }
+});
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
+
+
 
 io.on('connection', (socket) => {
   socket.on('chat message', msg => {
